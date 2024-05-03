92571b15587d931844e62dd4f6fc97ed5813be8a
illia <fialkivskiji@gmail.com>
2024-05-03 21:38:34 +0300

    default layout

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+    console.log('default layout')
+</script>
+
+<template>
+    <section class="grid" style="min-height: 100vh;">
+        <aside>Sidebar</aside>
+        <div>
+            <slot />
+        </div>
+    </section>
+</template>
+
+<style scoped>
+    .grid{
+        display: grid;
+        grid-template-columns: 1fr 4fr;
+    }
+</style>
