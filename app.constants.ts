9096bd9c9748a07eea370d3e940043236b4fa5bc
illia <fialkivskiji@gmail.com>
2024-05-08 18:38:55 +0300

    login page

--- app.constants.ts
+++ app.constants.ts
@@ -1,1 +1,9 @@
-export const APP_WRITE_ID:string = ""
+export const APP_WRITE_ID:string = "663b8ce1003cfb475950";
+
+export const COLLECTION_ID:string = "nuxt-base";
+export const COLLECTION_DEALS:string = "deals";
+export const COLLECTION_CUSTOMERS:string = "customers";
+export const COLLECTION_COMMENTS:string = "comments";
+
+
+export const STORAGE_ID:string = "storage";
---
+++ pages/login.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+
+useHead({
+    title: 'Login',
+})
+
+const emailRef = ref('')
+const passwordRef = ref('')
+const nameRef = ref('')
+</script>
+
+<template>
+	<div class="flex items-center justify-center min-h-screen w-full">
+		<div class="rounded bg-sidebar w-1/4 p-5">
+			<h1 class="text-2xl font-bold text-center mb-5">Login</h1>
+
+			<form>
+				<UiInput
+					placeholder="Email"
+					type="email"
+					class="mb-3"
+					v-model="emailRef"
+				/>
+				<UiInput
+					placeholder="Password"
+					type="password"
+					class="mb-3"
+					v-model="passwordRef"
+				/>
+				<UiInput
+					placeholder="Name"
+					type="name"
+					class="mb-3"
+					v-model="nameRef"
+				/>
+				<!-- <div class="flex items-center justify-center gap-5">
+					<UiButton type="button" @click="login">Login</UiButton>
+					<UiButton type="button" @click="register">Register</UiButton>
+				</div> -->
+			</form>
+		</div>
+	</div>
+</template>
